9e96a92be1a88f240913351dd71d799eefb4d48f
gauri23092002 <gauri23092002@gmail.com>
2022-07-04 23:48:42 +0530

    course viewed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddcourseComponent } from './addcourse/addcourse.component';
+import { ViewCourseComponent } from './view-course/view-course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddcourseComponent
+    AddcourseComponent,
+    ViewCourseComponent
   ],
   imports: [
     BrowserModule,
